17f1b8ff36a7947ead2111ef516e991fa98ad469
ashxvegeta <ashutoshroy453@gmail.com>
2025-10-23 20:47:20 +0530

    added tempretur to chamodels

--- 2.ChatModels/1_chatmodels_openai.py
+++ 2.ChatModels/1_chatmodels_openai.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 model = ChatOpenAI(
     model="gpt-4",
+    temperature=0.7
 ) # Initialize the OpenAI Chat LLM with the specified model
 result = model.invoke("what is the capital of india?")  # Call the Chat LLM to get the capital of India
 print(result.content)  # Print the result
