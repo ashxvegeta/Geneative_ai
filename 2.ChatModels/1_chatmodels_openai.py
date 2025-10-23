1f0905184f0a02d0869cb4d02e2c0ac7c1fce512
ashxvegeta <ashutoshroy453@gmail.com>
2025-10-23 20:45:32 +0530

    Initialize ChatOpenAI model and test prompt

---
+++ 2.ChatModels/1_chatmodels_openai.py
@@ -0,0 +1,9 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+model = ChatOpenAI(
+    model="gpt-4",
+) # Initialize the OpenAI Chat LLM with the specified model
+result = model.invoke("what is the capital of india?")  # Call the Chat LLM to get the capital of India
+print(result.content)  # Print the result
