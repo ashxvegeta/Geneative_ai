4f7fd59c7a6716aea0057e504791d6a7093b8677
ashxvegeta <ashutoshroy453@gmail.com>
2025-10-30 21:57:52 +0530

    Updated HuggingFace local chat model code

--- 2.ChatModels/5_chatmodel_huggingface_local.py
+++ 2.ChatModels/5_chatmodel_huggingface_local.py
@@ -1,16 +1,16 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-from transformers import pipeline
+# from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+# from transformers import pipeline
 
-pipe = pipeline(
-    "text-generation",
-    model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    temperature=0.5,
-    max_new_tokens=256,
-    device_map="cpu"
-)
+# pipe = pipeline(
+#     "text-generation",
+#     model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+#     temperature=0.5,
+#     max_new_tokens=256,
+#     device_map="cpu"
+# )
 
-llm = HuggingFacePipeline(pipeline=pipe)
-model = ChatHuggingFace(llm=llm)
+# llm = HuggingFacePipeline(pipeline=pipe)
+# model = ChatHuggingFace(llm=llm)
 
-result = model.invoke("Who is the prime minister of India?")
-print(result.content)
+# result = model.invoke("Who is the prime minister of India?")
+# print(result.content)
