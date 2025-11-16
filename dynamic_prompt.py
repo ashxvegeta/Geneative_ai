e1000c9028a6463eb13cfa069d4872612d91f93b
ashxvegeta <ashutoshroy453@gmail.com>
2025-11-16 15:06:15 +0530

    #Reuseable Templete

--- dynamic_prompt.py
+++ dynamic_prompt.py
@@ -1,9 +1,11 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
+# for old version of langchain
+# from langchain.prompts import PromptTemplate
 from langchain.schema import HumanMessage
 from dotenv import load_dotenv
 import streamlit as st
-
+# for new version of langchain
+from langchain_core.prompts import PromptTemplate,load_prompt
 load_dotenv()
 st.header("Research Tool")
 #  take user_input
@@ -16,23 +18,8 @@ style_input = st.selectbox("Select Explanation Style", ["Beginner-Friendly", "Te
 
 length_input = st.selectbox("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-# template for dynamic prompt
-template = PromptTemplate(
-  
-    template="""Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-- Include relevant mathematical equations if present in the paper.
-- Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-2. Analogies:
-- Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=["paper_input", "style_input"],// suppose u have forget to put the prameter length_input "length_input"
-validate_template=True,//for validating the template
-)
+# load the dynaic prompt
+template = load_prompt("template.json")  
 
 # placeholder for output
 prompt = template.invoke({"paper_input": paper_input, "style_input": style_input, "length_input": length_input})
