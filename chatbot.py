4fcf45e213809807a1fbec6839f94b23be69cfd1
ashxvegeta <ashutoshroy453@gmail.com>
2025-11-19 22:18:37 +0530

    chatbot with chathistory

--- chatbot.py
+++ chatbot.py
@@ -3,9 +3,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+# create an empty chat history list
+chat_history = [] 
 while True:
     user_input  = input('You: ')
+    # append user input to chat history
+    chat_history.append(user_input)
     if user_input == 'exit':
         break
-    response = model.invoke(user_input)
+    # get model response based on chat history
+    response = model.invoke(chat_history)
+    # append model response to chat history 
+    chat_history.append(response.content)
+    
     print("Bot:", response.content)
+
+print(chat_history)
