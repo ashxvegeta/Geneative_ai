b8b25a979f92210b76bfdc2ac9d674730051fbe5
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-17 22:54:15 +0530

    Default validation

--- pydanticdemo.py
+++ pydanticdemo.py
@@ -1,12 +1,13 @@
-from  pydantic import BaseModel
+from  pydantic import BaseModel,EmailStr
 from typing import Optional
 class Student(BaseModel):
     name: str = "John Doe"
     age: Optional[int] = None
+    email: Optional[EmailStr] = None
    
 # Creating raw input data (dictionary)
-# here u can see age is string but pydantic will convert it to int so this is called type coercion and in output age will be int
-new_student = {"age": "20"}
+# here i have put wrong email format to show validation
+new_student = {"age": "20" , "email": "ss.com"}
 
 # Creating a Student object from the dictionary
 Student = Student(**new_student)
