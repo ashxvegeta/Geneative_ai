751207804f0e4a02ef4d1770ce8e621953273009
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-17 21:43:54 +0530

    ste default value in pydantic

--- pydanticdemo.py
+++ pydanticdemo.py
@@ -8,4 +8,18 @@ new_student = {}
 
 # Creating a Student object from the dictionary
 Student = Student(**new_student)
-print(Student)  # Output: John Doe
+print(Student.name)  # Output: John Doe
+
+
+# ste default value in pydantic 
+# from  pydantic import BaseModel
+
+# class Student(BaseModel):
+#     name: str = "John Doe"
+   
+# # Creating raw input data (dictionary)
+# new_student = {}
+
+# # Creating a Student object from the dictionary
+# Student = Student(**new_student)
+# print(Student.name)  # Output: John Doe
