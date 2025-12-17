8944f5ad72d3e9b1e926d87b9800944396d386f1
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-17 21:51:33 +0530

    set optional values in pydantic

--- pydanticdemo.py
+++ pydanticdemo.py
@@ -1,25 +1,14 @@
 from  pydantic import BaseModel
-
+from typing import Optional
 class Student(BaseModel):
     name: str = "John Doe"
+    age: Optional[int] = None
    
 # Creating raw input data (dictionary)
 new_student = {}
 
 # Creating a Student object from the dictionary
 Student = Student(**new_student)
-print(Student.name)  # Output: John Doe
+print(Student)  # Output: John Doe
 
 
-# ste default value in pydantic 
-# from  pydantic import BaseModel
-
-# class Student(BaseModel):
-#     name: str = "John Doe"
-   
-# # Creating raw input data (dictionary)
-# new_student = {}
-
-# # Creating a Student object from the dictionary
-# Student = Student(**new_student)
-# print(Student.name)  # Output: John Doe
