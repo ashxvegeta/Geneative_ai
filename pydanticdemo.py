1a3c27371089a2f9137b5af448a3ee60926e6955
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-19 22:18:41 +0530

    Add Custom Description

--- pydanticdemo.py
+++ pydanticdemo.py
@@ -4,7 +4,7 @@ class Student(BaseModel):
     name: str = "John Doe"
     age: Optional[int] = None
     email: Optional[EmailStr] = None
-    cgpa: Optional[float] = Field(gt=0, lt=10,default=5)  # cgpa must be between 0 and 10
+    cgpa: Optional[float] = Field(gt=0, lt=10,default=5,description="CGPA must be between 0 and 10")  # cgpa must be between 0 and 10
 
 # Creating raw input data (dictionary)
 # here i have put wrong email format to show validation
