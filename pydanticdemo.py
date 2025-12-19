c88dcfdfb719d38a604e7fe04af8c68f3e4ab956
ashxvegeta <ashutoshroy453@gmail.com>
2025-12-19 22:15:20 +0530

    U can add default Cgpa like this

--- pydanticdemo.py
+++ pydanticdemo.py
@@ -4,11 +4,11 @@ class Student(BaseModel):
     name: str = "John Doe"
     age: Optional[int] = None
     email: Optional[EmailStr] = None
-    cgpa: Optional[float] = Field(gt=0, lt=10)  # cgpa must be between 0 and 10
+    cgpa: Optional[float] = Field(gt=0, lt=10,default=5)  # cgpa must be between 0 and 10
 
 # Creating raw input data (dictionary)
 # here i have put wrong email format to show validation
-new_student = {"age": "20" , "email": "ashu@gmail.com", "cgpa": 11.5}
+new_student = {"age": "20" , "email": "ashu@gmail.com"}
 
 # Creating a Student object from the dictionary
 Student = Student(**new_student)
