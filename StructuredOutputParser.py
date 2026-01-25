23b9b895fd2627c72e89b107824bb3331e5cb213
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-25 14:53:03 +0530

    Structured output Parser Using Schema

---
+++ StructuredOutputParser.py
@@ -0,0 +1,34 @@
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import ResponseSchema, StructuredOutputParser
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+)
+model = ChatHuggingFace(llm=llm,temperature=0)
+
+schemas = [
+    ResponseSchema(name="fact1", description="fact 1 about the topic"),
+    ResponseSchema(name="fact2", description="fact 2 about the topic"),
+    ResponseSchema(name="fact3", description="fact 3 about the topic"),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schemas)
+
+templete= PromptTemplate(
+    template="give me 3 facts about {topic} in the following format\n {fomat_instructions}" ,
+    input_variables=["topic"],
+    partial_variables={"fomat_instructions": parser.get_format_instructions()}
+)
+
+prompt = templete.invoke({"topic": "climate change"})
+
+result = model.invoke(prompt)
+
+final_result = parser.parse(result.content)
+
+print("Final Result:", final_result)
